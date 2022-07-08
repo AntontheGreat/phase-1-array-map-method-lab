0ac0ef83786efd271d79e4be20e3ba840f3f3d1c
Antonio <antonio18casta@aol.com>
2022-07-08 14:34:05 -0400

    commit tree 7af0f09ed597f3a662ef8d06e65f2cd96f88a015

--- index.js
+++ index.js
@@ -12,5 +12,35 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const arrayOfWords = tutorials.map(tutorial => tutorial.split(" ").map(e => e[0].toUpperCase()+e.slice(1)).join(" "))
+  //console.log(arrayOfWords)
+  //const result = arrayOfWords.map(e => e[0].toUpperCase()+e.slice(1));
+  // console.log(arrayOfWords)
+  // const newSentence = arrayOfWords.join(" ")
+  // console.log(newSentence)
+  //return result
+  return arrayOfWords
 }
+
+// const stringSplit = tutorials.split(" ");
+// tutorials.charAt(0)
+
+// function capitalize() {
+//   // const words = sentence.split(" ");
+//   const arrayOfWords = tutorials.map(tutorial => tutorial.split(" "))
+//   console.log(arrayOfWords)
+//   const result = words.map(e => e[0].toUpperCase()+e.slice(1));
+//   const newSentence = result.join(" ")
+//   return newSentence
+// }
+
+
+//.map(first => first[0].toUpperCase() + first.substring(1)).join(" ");
+
+//const defaultString = 'what does the this keyword mean?'
+
+// const stringSplit = defaultString.split(" ");
+
+// console.log(stringSplit);
+
+// stringSplit.charAt(0)
